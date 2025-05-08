6e859eb5c06bb6decdca7fae5eba296b5877a531
UnderDoug <douglas.marken@gmail.com>
2025-05-08 10:10:53 +1000

    Add project files.

---
+++ README.md
@@ -0,0 +1,25 @@
+With this mod, everyone's favourite seething mound of malachine spite is pettable!
+
+_Should_ you pet him? Absolutely not!
+
+Definitely not a second time, either! _No really. Don't._
+
+### Steps to pet Mak
+1. Approach Mak.
+2. Think about petting Mak.
+3. ??????
+4. DON'T
+
+### FAQ
+
+Q: Can I pet Mak with this mod enabled?
+A: _Yes! but I can't imagine why you'd want to._
+
+Q: Why did Mak tell me off when I tried to pet him?
+A: _Have you spoken to him? He's not a friendly frog._
+
+Q: I petted him a second time, and _the thing_ happened :(
+A: _Not a question, but he did warn you not to pet him again._
+
+Q: Why did you make this mod??
+A: _Good question!_
---
+++ UD_DoNotPetMe.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using XRL.Language;
+
+using SerializeField = UnityEngine.SerializeField;
+
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class UD_DoNotPetMe : IScribedPart
+    {
+        public List<string> HasWarned = new();
+        public string WarnMessage;
+        public string WarnFilter;
+        public string AngerMessage;
+        public string ConsumeFloatMessage;
+
+        public UD_DoNotPetMe() 
+        {
+            WarnMessage = "Best not be doing that gain!";
+            AngerMessage = "YOU WERE WARNED";
+        }
+
+        public override bool WantEvent(int ID, int cascade)
+        {
+            return base.WantEvent(ID, cascade)
+                || ID == AfterObjectCreatedEvent.ID
+                || ID == AfterPetEvent.ID;
+        }
+
+        public override bool HandleEvent(AfterObjectCreatedEvent E)
+        {
+            if (E.Object == ParentObject)
+            {
+                Pettable pettable = ParentObject.RequirePart<Pettable>();
+                pettable.OnlyAllowIfLiked = false;
+                pettable.MaxAIDistance = 1;
+                ParentObject.SetIntProperty("PetGoalWait", 999999);
+                ParentObject.SetStringProperty("PreferChatToPet", "He will literally eat you");
+            }
+            return base.HandleEvent(E);
+        }
+
+        public override bool HandleEvent(AfterPetEvent E)
+        {
+            if (!HasWarned.Contains(E.Actor.ID))
+            {
+                HasWarned.Add(E.Actor.ID);
+                bool allowSecondPerson = Grammar.AllowSecondPerson;
+                Grammar.AllowSecondPerson = false;
+                string message = GameText.VariableReplace(WarnMessage, ParentObject, E.Actor);
+                Grammar.AllowSecondPerson = allowSecondPerson;
+                EmitMessage(TextFilters.Filter(message, WarnFilter), ' ', FromDialog: false, E.Actor.IsPlayer() || ParentObject.IsPlayer());
+            }
+            else
+            {
+                bool allowSecondPerson = Grammar.AllowSecondPerson;
+                Grammar.AllowSecondPerson = false;
+                string message = GameText.VariableReplace(AngerMessage, ParentObject, E.Actor);
+                Grammar.AllowSecondPerson = allowSecondPerson;
+                EmitMessage(message, ' ', FromDialog: false, E.Actor.IsPlayer() || ParentObject.IsPlayer());
+
+                Consumer consumer = ParentObject.RequirePart<Consumer>();
+                consumer.WeightThresholdPercentage = 200;
+                consumer.Message = "{{R|=subject.T= =verb:swallow= =object.Name= whole for petting =pronouns.objective= too many times!}}";
+                consumer.FloatMessage = ConsumeFloatMessage;
+
+                if (consumer.CanConsume(E.Actor) && !consumer.ShouldIgnore(E.Actor))
+                {
+                    E.Actor.Render.RenderLayer = E.Object.Render.RenderLayer - 1;
+                    E.Actor.DirectMoveTo(E.Object.CurrentCell, 0, true, true, true);
+                    consumer.Consume(E.Actor);
+                    return false;
+                }
+            }
+            return base.HandleEvent(E);
+        }
+    }
+}
